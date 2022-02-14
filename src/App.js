2fe6a94a5b3290abc1e1bb71eed1626e04845631
Mandy Yu <yumandee@yahoo.com>
2022-02-13 19:31:14 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import memories from './images/memories.png';
+
+const App = () => {
+   return (
+      <Container maxwidth="lg">
+         <AppBar position="static" color="inherit">
+            <Typography variant="h2" align="center">Memories</Typography>
+            <img src={memories} alt="memories" height="60" />
+         </AppBar>
+         <Grow in>
+            <Container>
+               <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                  <Grid item xs={12} sm={7}>
+                     <Posts />
+                  </Grid>
+                  <Grid item xs={12} sm={4}>
+                     <Form />
+                  </Grid>
+               </Grid>
+            </Container>
+         </Grow>
+
+      </Container>
+   )
 }
 
 export default App;
