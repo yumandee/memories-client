6d2c8d5cd779818d0e7d2f38776388fb66b37dcd
Mandy Yu <yumandee@yahoo.com>
2022-02-18 22:53:43 -0500

    action type constants added

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,15 +1,14 @@
 /* eslint-disable import/no-anonymous-default-export */
-// import { FETCH_ALL, CREATE } from '../cons'
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
 export default (posts = [], action) => {
    switch (action.type) {
-      case 'UPDATE':
-      case 'LIKE':
+      case UPDATE:
          return posts.map((post) => post._id === action.payload._id ? action.payload : post)
-      case 'FETCH_ALL':
+      case FETCH_ALL:
          return action.payload;
-      case 'CREATE':
+      case CREATE:
          return [...posts, action.payload];
-      case 'DELETE':
+      case DELETE:
          return posts.filter((post) => post._id  !== action.payload)
       default: 
          return posts;
